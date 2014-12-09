c019d0847d10d93522f86f18b30b6bc9568b9eb2
Prachi Ghatwal <prachi.ghatwal@creativecapsule.com>
2014-12-09 17:41:36 +0530

    [Fixes #84199498] label aligned to top of the view

---
+++ TestPivotalTrack/TestPivotalTrack/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestPivotalTrack
+//
+//  Created by Prachi Ghatwal on 09/12/14.
+//  Copyright (c) 2014 Prachi Ghatwal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
